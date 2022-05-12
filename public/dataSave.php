0009b9a08894250c8e7b81551e5836fce7d97d61
timbarden <hello@tim-barden.co.uk>
2022-05-12 13:26:50 +0100

    Update dataSave.php

--- public/dataSave.php
+++ public/dataSave.php
@@ -19,33 +19,4 @@ mysqli_close($con);
 
 
 // consider switch to PDO
-
-
-/* $growlist = $_POST["growlist"];
-$username = $_POST["username"];
-
-
-$host = 'localhost';
-$db   = 'wpwxqzcc_plotbook';
-$user = 'wpwxqzcc_plotbookuser';
-$pass = 'abtG5dazrzySgS8';
-$port = "3306";
-$charset = 'utf8mb4';
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    $query = "UPDATE users SET data = $growlist WHERE username = 'timbarden@outlook.com'";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute(array($data, $username));
-    echo "yep";
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-    echo "nope";
-} */
 ?>
